94d2d38ba3fd75d54fd6bee82ae4833ca4deb16a
Fang1217 <57334019+Fang1217@users.noreply.github.com>
2025-09-02 03:20:21 +0800

    feat: static content routes

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin 
 from django.urls import path 
-from .views import sayHello 
-  
+from . import views
+
 urlpatterns = [ 
-    path('', sayHello, name='sayHello'), 
+    path('', views.index, name='index'), 
 ]
